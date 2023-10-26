19e297919cb1f96f813c027e7b68b546f7142ccb
Afsha10 <unityofbeing1@gmail.com>
2023-10-26 04:39:03 +0100

    Feat: Add components to App.js, create header and footer components
    * Add components to App.js: Header, MainContainer, and Footer
    * Create a header component
    * Create a footer component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,13 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import MainContainer from "./components/MainContainer";
+
 function App() {
   return (
-    <div className="text-3xl font-bold text-center">
-      Hello World
+    <div className="bg-gradient-to-tl from-pink-300 via-purple-300 to-purple-300">
+      <Header />
+      <MainContainer />
+      <Footer />
     </div>
   );
 }
---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <div>
+        <h1 className=" text-xl sm:text-4xl font-bold mx-3 mt-4 text-fuchsia-950 text-center">
+          Recommendations Tracker
+        </h1>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
