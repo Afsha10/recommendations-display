12a322cad756a375cbd26fa01c2e7ea803f33633
Afsha10 <unityofbeing1@gmail.com>
2023-10-26 22:43:01 +0100

    Chore: Change baseUrl to newly deployed server URL

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,3 +1,3 @@
-// export const baseUrl = ""; // prod
+export const baseUrl = "https://recommendations-tracker-backend.onrender.com/"; // prod
 
-const baseUrl = "http://localhost:5000"; //dev
+// const baseUrl = "http://localhost:5000"; //dev
