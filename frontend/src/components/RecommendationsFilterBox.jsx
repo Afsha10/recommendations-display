9a6a1c87f3ac02b606c1c80509a50ece029137f3
Afsha10 <unityofbeing1@gmail.com>
2023-10-27 19:10:49 +0100

    Feat: Create RecommendationsFilterBox Component

---
+++ frontend/src/components/RecommendationsFilterBox.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function RecommendationsFilterBox() {
+  return (
+    <div>
+        <h2 className="text-xl text-center font-bold">Filter your recommendations by... </h2>
+    </div>
+  )
+}
+
+export default RecommendationsFilterBox
