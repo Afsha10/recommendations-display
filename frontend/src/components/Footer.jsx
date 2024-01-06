b91f6847a53ba0be772699741c3ff99d4d2edf93
Afsha10 <unityofbeing1@gmail.com>
2024-01-06 22:00:50 +0000

    Change copyright year to 2024

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <div>
       <footer className="bg-fuchsia-950 p-4 text-center text-xs text-white w-screen mt-auto">
-        <p>&copy; 2023 Recommendations Tracker created by Afsha with ❤️</p>
+        <p>&copy; 2024 Recommendations Tracker created by Afsha with ❤️</p>
       </footer>
     </div>
   );
