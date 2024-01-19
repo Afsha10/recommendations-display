4b6fa829066b2caa1a1a1f56bd260f1fc9420a00
Afsha10 <unityofbeing1@gmail.com>
2024-01-19 01:37:44 +0000

    Connect the media and moods filter menus to their respective tables in the database
    * Install axios and fetch data from media and moods tables into the media and moods filter drop downs
    * Create FilterDropDown with an option for all media and all moods
    * Use useState for media and moods to populate the media and moods drop downs with their respective tables from the database so that we don't need to hard code the option values

--- frontend/src/components/FilterDropDown.jsx
+++ frontend/src/components/FilterDropDown.jsx
@@ -7,9 +7,20 @@ const FilterDropDown = ({ type, values }) => {
   return (
     <div className="bg-fuchsia-900 gap-4">
       <label for={type}>Choose a {type}: </label>
-      <select className="text-gray-950  mx-1 my-2 p-2" id={type} name={type}>
+      <select
+        className="text-gray-950  mx-1 my-2 p-2"
+        id={type}
+        name={type}
+        // onChange={handleOnChange}
+      >
+        <option value="">All {type}</option>
         {values.map((val) => {
-          return <option value={val}>{val}</option>;
+          console.log("val", val);
+          return (
+            <option key={val} value={val}>
+              {val}
+            </option>
+          );
         })}
       </select>
     </div>
--- frontend/src/components/RecommendationsFilterBox.jsx
+++ frontend/src/components/RecommendationsFilterBox.jsx
@@ -1,33 +1,63 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import FilterDropDown from "./FilterDropDown";
+import axios from "axios";
 
-function RecommendationsFilterBox({ media, setMedia }) {
+function RecommendationsFilterBox() {
+  const [media, setMedia] = useState([]);
+  const [moods, setMoods] = useState([]);
+  // const handleOnchange = () => {};
+
+  useEffect(() => {
+    const fetchMedia = async () => {
+      try {
+        const response = await axios.get(
+          "https://recommendations-tracker-backend.onrender.com/media"
+        );
+        setMedia(
+          response.data.map((val) => {
+            return val.medium_type;
+          })
+        );
+      } catch (error) {
+        console.log("error");
+      }
+    };
+    fetchMedia();
+  }, []);
+
+  useEffect(() => {
+    const fetchMoods = async () => {
+      try {
+        const response = await axios.get(
+          "https://recommendations-tracker-backend.onrender.com/moods"
+        );
+        setMoods(
+          response.data.map((val) => {
+            return val.mood_type;
+          })
+        );
+      } catch (error) {
+        console.log("error");
+      }
+    };
+    fetchMoods();
+  }, []);
+
   return (
     <div>
       <h2 className="text-xl text-center font-bold my-2">
         Filter your recommendations by...
       </h2>
       <div className="md:grid grid-cols-3 bg-fuchsia-900 p-6 text-white m-5">
+        {/* medium filter drop down */}
         <FilterDropDown
-          type="medium"
-          values={["All media", "Books", "Movies/Films", "Events", "Music"]}
+          type="media"
+          // values={["All media", "Books", "Movies/Films", "Events", "Music"]}
+          values={media}
+          // handleOnChange={handleOnChange}
         />
-        <FilterDropDown
-          type="mood"
-          values={["All moods", "Happy", "Sad", "Inspiration", "Whimsical"]}
-        />
-        {/* How do we fix formatting */}
-        <FilterDropDown
-          type="recommender"
-          values={[
-            "Everyone's recommendations",
-            "CathyRose",
-            "LorentzTulip",
-            "AlexeyOrchid",
-            "AlishaJasmine",
-            "DanielLily",
-          ]}
-        />
+        {/* mood filter drop down */}
+        <FilterDropDown type="moods" values={moods} />
       </div>
     </div>
   );
